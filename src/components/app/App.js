02b875fe60fb6a1d92c6e4cd1bd9afa6d738aa16
Muratov <timuratov@mail.ru>
2021-12-12 17:46:29 +0700

    Add react-router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,56 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import PropTypes from 'prop-types';
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
+import {MainPage, ComicsPage} from '../pages';
 
 const App = () => {
     
-    const [selectedChar, setChar] = useState(null);
-    const [page, setPage] = useState('Char');
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
-    const onPageChoose = (page) => {
-        setPage(page);
-    }
-
-    let mainInfo;
-    if (page === 'Char') {
-        mainInfo = (
-        <>
-        <ErrorBoundary> 
-            <RandomChar/>
-        </ErrorBoundary>        
-        <div className="char__content">
-                <CharList onCharSelected={onCharSelected}/>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-                </ErrorBoundary>                    
-            </div>
-        </>     
-    );
-    } else if ('Comics'){
-        mainInfo = (
-            <>
-                <AppBanner/>
-                <ComicsList onCharSelected={onCharSelected}/>   
-            </>
-        );
-    }
     return (
-        <div className="app">
-            <AppHeader page={page} setPage={onPageChoose}/>
-            <main>
-                {mainInfo}
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>                            
+                        <Route path="/comics" element={<ComicsPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     )
 }
 
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,19 +1,22 @@
 import './appHeader.scss';
+import {Link, NavLink} from 'react-router-dom';
 
 const AppHeader = (props) => {
 
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="#">
+                <Link to="/">
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li onClick={() => props.setPage('Char')}><a href="#">Characters</a></li>
+                    <li><NavLink end 
+                    style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})} to="/">Characters</NavLink></li>
                     /
-                    <li onClick={() => props.setPage('Comics')}><a href="#">Comics</a></li>
+                    <li><NavLink end 
+                    style={({isActive}) => ({color: isActive ? '#9f0013' : 'inherit'})} to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
